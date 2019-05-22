503b2ccb1c6d9ab64fcda62180903e7a4bb654b9
Erik <kirekire95@gmail.com>
2019-05-22 22:42:55 +0200

    more changes

--- src/pages/lazyload.js
+++ src/pages/lazyload.js
@@ -1,26 +1,26 @@
-import React from "react"
-import { graphql } from "gatsby"
-import Img from "gatsby-image"
+// import React from "react"
+// import { graphql } from "gatsby"
+// import Img from "gatsby-image"
 
-export const pageQuery = graphql`
-  query HeaderImageQuery {
-    headerImage: imageSharp(id: { regex: "/header/" }) {
-      sizes(maxWidth: 1240) {
-        ...GatsbyImageSharpSizes
-      }
-    }
-  }
-`
+// export const pageQuery = graphql`
+//   query HeaderImageQuery {
+//     headerImage: imageSharp(id: { regex: "/header/" }) {
+//       sizes(maxWidth: 1240) {
+//         ...GatsbyImageSharpSizes
+//       }
+//     }
+//   }
+// `
 
-const LazyLoad = (props) => {
-  return (
-    <Img
-      className="InsertedImage"
-      title="title"
-      alt="ha"
-      sizes={props.headerImage.sizes}
-    />
-  )
-}
+// const LazyLoad = (props) => {
+//   return (
+//     <Img
+//       className="InsertedImage"
+//       title="title"
+//       alt="ha"
+//       sizes={props.headerImage.sizes}
+//     />
+//   )
+// }
 
-export default LazyLoad
+// export default LazyLoad
